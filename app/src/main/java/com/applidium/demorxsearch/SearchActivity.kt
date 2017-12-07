b6728cd86933adbee69f1e5b3e8156adf524443b
Vincent Brison <vincent.brison@applidium.com>
2017-12-07 15:09:28 +0100

    Initial import with dummy search view

---
+++ app/src/main/java/com/applidium/demorxsearch/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.applidium.demorxsearch
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
